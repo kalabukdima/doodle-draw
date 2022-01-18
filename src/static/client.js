b8e2f460d510c0ca3c4539ba3c3c797c0bcdd6a2
Kalabuk Dmitriy <kalabukdima@gmail.com>
2022-01-18 10:15:54 +0300

    Implement client behavior

--- src/static/client.js
+++ src/static/client.js
@@ -1,31 +1,83 @@
-const socket = io();
+class State {
+    onNewPos(_data) {}
+    onMovement(_direction) {}
+    onStopMovement() {}
+}
 
-const canvas = document.querySelector('canvas');
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+class UninitializedState extends State {
+}
 
-const context = canvas.getContext('2d');
-const radius = 10;
+class ActiveState extends State {
+    constructor(socket, context, params) {
+        super();
+        this.socket = socket;
+        this.context = context;
+        this.allowedMoves = params.allowed;
+        this.lastPos = {x: params.pos.x, y: params.pos.y};
+    }
 
-// form.addEventListener('submit', function (e) {
-//     e.preventDefault();
-//     if (input.value) {
-//         socket.emit('chat message', input.value);
-//         input.value = '';
-//     }
-// });
+    onNewPos(data) {
+        const newPos = {x: data.x, y: data.y};
+        this.context.beginPath();
+        this.context.moveTo(this.lastPos.x, this.lastPos.y);
+        this.context.lineTo(newPos.x, newPos.y);
+        this.context.stroke();
+        this.lastPos = newPos;
+    }
 
-socket.on('coords', function (data) {
-    context.clearRect(0, 0, canvas.width, canvas.height);
+    onMovement(direction) {
+        this.socket.emit(direction);
+    }
 
-    console.log(data)
-    console.log(canvas.width * data.x)
-    context.beginPath();
-    context.arc(canvas.width * data.x, canvas.height * data.y, radius, 0, 2 * Math.PI, false);
-    context.fillStyle = 'green';
-    context.fill();
-    context.lineWidth = 5;
-    context.strokeStyle = '#003300';
-});
+    onStopMovement() {
+        this.socket.emit("stop");
+    }
+}
 
+function run() {
+    const socket = io();
 
+    const canvas = document.querySelector("canvas");
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    const context = canvas.getContext("2d");
+    context.lineWidth = 3;
+
+    let state = new UninitializedState();
+
+    socket.on("init", (params) => {
+        console.log("initialized");
+        state = new ActiveState(socket, context, params);
+    });
+
+    socket.on("new_pos", (data) => {
+        state.onNewPos(data);
+    })
+
+    document.addEventListener("keydown", (event) => {
+        if (event.repeat) {
+            return;
+        }
+        switch (event.key) {
+            case "ArrowUp":
+                state.onMovement("up");
+                break;
+            case "ArrowDown":
+                state.onMovement("down");
+                break;
+            case "ArrowLeft":
+                state.onMovement("left");
+                break;
+            case "ArrowRight":
+                state.onMovement("right");
+                break;
+        }
+    }, false);
+
+    document.addEventListener("keyup", (_event) => {
+        state.onStopMovement();
+    }, false);
+}
+
+run();
